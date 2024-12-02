411c2a6cf89353a70331812845e57f484957f762
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2024-12-02 00:45:18 -0500

    rides page google oAuth

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useUser } from "@clerk/clerk-expo";
+import { useFetch } from "@/lib/fetch";
+import { Ride } from "@/types/type";
+import { images } from "@/constants";
+import RideCard from "@/components/RideCard";
 
 const rides = () => {
+  const { user } = useUser();
+  const { data: recentRides, loading } = useFetch<Ride[]>(
+    `/(api)/ride/${user?.id}`
+  );
   return (
-    <View>
-      <Text>rides</Text>
-    </View>
+    <SafeAreaView className="h-full border w-full flex items-center justify-center z-0">
+      <FlatList
+        data={recentRides}
+        // data={[]}
+        renderItem={({ item }) => <RideCard item={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  resizeMode="contain"
+                  className="w-40 h-40"
+                />
+                <Text className="text-sm">No rides founded</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <Text className="text-2xl font-JakartaBold my-5">All Rides</Text>
+          </>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
 export default rides;
-
-const styles = StyleSheet.create({});
